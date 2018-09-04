d1a729f42ce19272716d49769cf34cf95cebac78
Robert Johns <johnsr10stc@gmail.com>
2018-09-04 18:08:18 -0400

    added BinarySearchTree.py

---
+++ InterviewPreparation/WarmUp/BinarySearchTree.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+def lca(root, v1, v2):
+
+    big = max(v1, v2)
+    small = min (v1, v2)
+    
+    done = False
+    node = root
+    
+    while not done:
+        if big == node.info or small == node.info:
+            done = True
+        elif big > node.info and small < node.info:
+            done = True
+        elif big > node.info and small > node.info:
+            node = node.right
+        else:
+            node = node.left
+            
+    return node
+
+          
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+v = list(map(int, input().split()))
+
+ans = lca(tree.root, v[0], v[1])
+print (ans.info)    
